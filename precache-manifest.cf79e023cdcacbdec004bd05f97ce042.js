943d22596c1b230eaca722b1058782267ef37daa
Agzamova <agzamova@inbox.ru>
2020-09-21 01:38:16 +0500

    Updates

---
+++ precache-manifest.cf79e023cdcacbdec004bd05f97ce042.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cd8368fc73d2e8838dfe05e012319db",
+    "url": "/comments-app-redux/index.html"
+  },
+  {
+    "revision": "6b29e6925da9bb80bf04",
+    "url": "/comments-app-redux/static/css/main.6350144e.chunk.css"
+  },
+  {
+    "revision": "8a30e92ead3754624602",
+    "url": "/comments-app-redux/static/js/2.499e7044.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/comments-app-redux/static/js/2.499e7044.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b29e6925da9bb80bf04",
+    "url": "/comments-app-redux/static/js/main.8d4fe34b.chunk.js"
+  },
+  {
+    "revision": "f6dab7ef542544d06b95",
+    "url": "/comments-app-redux/static/js/runtime-main.dca1c2f8.js"
+  }
+]);
