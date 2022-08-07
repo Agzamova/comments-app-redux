40a0308635ee951c2ff285a2788a3ce7f696822f
Agzamova <agzamova@inbox.ru>
2022-08-07 13:43:05 +0500

    Updates

---
+++ precache-manifest.8432b45e03c6487c986611df54a40435.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c46ef983e366a68ab22fb06d01b98d64",
+    "url": "/comments-app-redux/index.html"
+  },
+  {
+    "revision": "0bf5e002a2a29ba38579",
+    "url": "/comments-app-redux/static/css/main.358da3cc.chunk.css"
+  },
+  {
+    "revision": "aa99d47182b016dfb812",
+    "url": "/comments-app-redux/static/js/2.38fb5ad7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/comments-app-redux/static/js/2.38fb5ad7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bf5e002a2a29ba38579",
+    "url": "/comments-app-redux/static/js/main.0aa8a6ff.chunk.js"
+  },
+  {
+    "revision": "ab51ab2db5aa5e5c2018",
+    "url": "/comments-app-redux/static/js/runtime-main.0c2bb0d7.js"
+  }
+]);
